5fb84adcd347351dc4415420858573961dec83d1
openscript <1105080+openscript@users.noreply.github.com>
2018-06-17 12:12:58 +0200

    Add entry service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './components/entry-list/entry-list.component';
+import { EntryFormComponent } from './components/entry-form/entry-form.component';
+import { EntryContainerComponent } from './container/entry-container/entry-container.component';
+import { EntryComponent } from './components/entry/entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent,
+    EntryFormComponent,
+    EntryContainerComponent,
+    EntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBase: 'https://ebzuerich-messages.herokuapp.com'
 };
